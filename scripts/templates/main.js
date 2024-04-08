9948221be7e317cd9c9cadf88dcafd13563062de
das422 <simonin974@gmail.com>
2024-04-08 11:19:42 +0400

    Update CSS and HTML files to improve styling and layout

---
+++ scripts/templates/main.js
@@ -0,0 +1,38 @@
+export default class RecipeCard {
+  constructor(recipe) {
+    this.recipe = recipe;
+  }
+
+  createRecipeCard() {
+    const card = document.createElement("div");
+    card.classList.add("recipe-card");
+    const RecipeCard = `
+
+    <div class="recipe-card-image">
+      <img src="./assets/images/${this.recipe.image}" alt="${this.recipe.name}">
+    </div>
+
+    <h3 class="font-bold">${this.recipe.name}</h3>
+    <div class="recipe-card-content -3">
+
+     <h4 class="text-muted uppercase text-sm">recette</h4>
+      <p class="text-wrap text-ellipsis"> ${this.recipe.description}</p>
+    </div>
+     <p>Ingredients</p>
+      <ul class="grid grid-cols-3">
+      
+        ${this.recipe.ingredients
+          .map(
+            (ingredient) => `
+          <li>${ingredient.ingredient}</li>
+          <li>${ingredient.quantity}</li>`
+          )
+          .join("")}
+      </ul>
+    </div>
+
+   `;
+    card.innerHTML = RecipeCard;
+    return card;
+  }
+}
